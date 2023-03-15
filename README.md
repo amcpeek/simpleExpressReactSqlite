51429210e2df1ff9332c98414b2749c2dcdc3b8c
amcpeek <ammcpeek@gmail.com>
2023-03-14 22:16:46 -0700

    create, read, delete functioning

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # simpleExpressReactSqlite
+
+
+# To Set Up the SQLite db
+
+sqlite3 dev.db
+
+CREATE TABLE coffee (
+   id INTEGER PRIMARY KEY,
+   name TEXT NOT NULL,
+   description TEXT NOT NULL
+);
+
+INSERT INTO coffee (name, description) VALUES
+  ("Espresso", "A strong and concentrated coffee, usually served in small shots."),
+  ("Cappuccino", "An espresso-based drink topped with frothed milk and a sprinkle of cocoa powder."),
+  ("Latte", "A milky coffee made with espresso and steamed milk."),
+  ("Americano", "A diluted espresso, usually served in a larger cup."),
+  ("Mocha", "A latte with added chocolate syrup or powder."),
+  ("Macchiato", "An espresso with a small amount of frothed milk on top."),
+  ("Iced coffee", "A cold coffee drink, usually served with ice and milk."),
+  ("French press", "A coffee-making method where coffee grounds are steeped in hot water and then filtered."),
+  ("Pour over", "A coffee-making method where hot water is slowly poured over coffee grounds in a filter."),
+  ("Turkish coffee", "A coffee-making method where finely ground coffee is boiled in a pot with water and sugar.");
--- simpleExpressBackend/app.js
+++ simpleExpressBackend/app.js
@@ -3,7 +3,10 @@ const express = require('express')
 const app = express()
 const port = 3001
 const cors=require("cors");
-const db = require('better-sqlite3')('quotes.db');
+const db = require('better-sqlite3')('dev.db');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 const corsOptions ={
    origin:'*',
    credentials:true,            //access-control-allow-credentials:true
@@ -15,8 +18,29 @@ app.use(cors(corsOptions)) // Use this after the variable declaration
 app.get('/', (req, res) => {
   // res.send('This should go from the backend to the frontend')
   const row = db.prepare('SELECT * FROM coffee').all();
-  console.log('what is row?', row)
+  // console.log('what is row?', row)
   res.json({message: row})
+})
+
+// app.post('/', (req, res) => {
+//   const {name, description} = req.body
+//   const inserted = db.prepare(`INSERT INTO coffee (name, description) VALUES (${req.body.name}, ${req.body.description})`);
+//   res.json({message: "success!"})
+// })
+app.post('/', (req, res) => {
+  const { name, description } = req.body;
+  // console.log('why not req body', req)
+  const inserted = db.prepare(`INSERT INTO coffee (name, description) VALUES ('${name}', '${description}')`);
+  inserted.run();
+  res.json({ message: "success!" });
+});
+
+app.delete('/:id', async(req, res) => {
+  const coffeeId = req.params.id
+  // console.log('just checking we are getting to the backend', coffeeId)
+  const coffeeToDelete = db.prepare(`DELETE FROM coffee WHERE id = ${coffeeId};`)
+  coffeeToDelete.run();
+  res.json({ message: "success deleting!" });
 })
 
 
@@ -24,3 +48,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// INSERT INTO coffee (name, description)
+// VALUES ('Black', 'just coffee'), ('Latte', 'espresso and steamed milk'), ('Cappuccino', 'espresso steamed milk and foam');
